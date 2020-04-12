727df0d68daa751d9a5e38b10a287c0ac3275cc6
JD <juandiegogarita@gmail.com>
2020-04-11 18:23:13 -0600

    main activity setup

--- app/src/main/java/com/jd/dailyplanet/ui/MainActivity.java
+++ app/src/main/java/com/jd/dailyplanet/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.jd.dailyplanet;
+package com.jd.dailyplanet.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.jd.dailyplanet.R;
 
 public class MainActivity extends AppCompatActivity {
 
